58bcb9f8677707f7d8ee4f9d7b9ca0b47e2850b7
afiyakouser07 <afiyakouser07@gmail.com>
2026-06-03 16:19:04 +0530

    Update analysis.py

--- notebooks/analysis.py
+++ notebooks/analysis.py
@@ -10,3 +10,19 @@ import pandas as pd
 import numpy as np
 import matplotlib.pyplot as plt
 import seaborn as sns
+# -----------------------------------------
+# Create visuals folder if not exists
+# -----------------------------------------
+
+os.makedirs("../visuals", exist_ok=True)
+
+# -----------------------------------------
+# Extract ZIP File
+# -----------------------------------------
+
+zip_path = "../archive.zip"
+
+extract_path = "../data"
+
+# Create data folder if not exists
+os.makedirs(extract_path, exist_ok=True)
